b0086f73e6d734d7f703e182d751ee2b8fa39a46
guanjieye <duolong>
2023-07-17 06:36:51 +0800

    init1

--- mch-core-web/src/main/java/cn/rto/mch/core/controllers/TestController.java
+++ mch-core-web/src/main/java/cn/rto/mch/core/controllers/TestController.java
@@ -1,10 +1,17 @@
 package cn.rto.mch.core.controllers;
 
-import cn.rto.mch.core.controllers.base.BaseResponse;
-import cn.rto.mch.core.manager.redis.RedisManager;
+import cn.sino.common.vo.RestResponseVO;
+import org.apache.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import cn.rto.mch.core.manager.redis.RedisManager;
+
+import java.net.URI;
 
 /**
  * ClassName: TestController
@@ -21,8 +28,18 @@ public class TestController {
 
     @GetMapping("/get")
     @ResponseBody
-    public ResponseEntity<Object> getTest(){
-        return BaseResponse.success("success", redisManager.increment("test_key1", 1L));
+    public RestResponseVO<Long> getTest() {
+        return RestResponseVO.success(redisManager.increment("test_key1", 1L));
+    }
+
+    @GetMapping("/get1")
+    public ResponseEntity<Long> getTest1() {
+        return ResponseEntity.status(HttpStatus.SC_OK).contentType(MediaType.APPLICATION_JSON_UTF8).body(redisManager.increment("test_key1", 1L));
+    }
+
+    @GetMapping("/get_redirect")
+    public ResponseEntity<Long> get_redirect() {
+        return ResponseEntity.status(HttpStatus.SC_MOVED_TEMPORARILY).location(URI.create("http://www.baidu.com")).build();
     }
 }
 
